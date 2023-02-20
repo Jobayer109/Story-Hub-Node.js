4261120a51c0dd8ec5aa7963ac6c2f2be4db2e89
user.email <jobayer.ahmed109@gmail.com>
2023-02-20 21:58:08 +0600

    app setup and database connection

---
+++ app.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+const morgan = require("morgan");
+const { engine } = require("express-handlebars");
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Load config
+dotenv.config({ path: "./config/config.env" });
+
+// Logging
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
+// Express - handlebars
+app.engine(".hbs", engine({ defaultLayout: "main", extname: ".hbs" }));
+app.set("view engine", ".hbs");
+
+// Server run
+const startDB = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(
+      PORT,
+      console.log(`server running in ${process.env.NODE_ENV} mode on port http://localhost:${PORT}`)
+    );
+    console.log("Database connected");
+  } catch (error) {
+    console.log(error.message);
+    process.exit(1);
+  }
+};
+startDB();
