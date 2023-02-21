21dbb57096d11f9027f37e787120d3747b3db654
user.email <jobayer.ahmed109@gmail.com>
2023-02-22 00:13:06 +0600

    Display single story

--- routes/stories.js
+++ routes/stories.js
@@ -36,6 +36,29 @@ router.get("/", confirmAuth, async (req, res) => {
   }
 });
 
+// @desc    Show single story
+// @route   GET /stories/:id
+router.get("/:id", confirmAuth, async (req, res) => {
+  try {
+    let story = await Story.findById(req.params.id).populate("user").lean();
+
+    if (!story) {
+      return res.render("error/404");
+    }
+
+    if (story.user._id != req.user.id && story.status == "private") {
+      res.render("error/404");
+    } else {
+      res.render("stories/show", {
+        story,
+      });
+    }
+  } catch (err) {
+    console.error(err);
+    res.render("error/404");
+  }
+});
+
 // @desc    Show edit page
 // @route   GET /stories/edit/:id
 router.get("/edit/:id", confirmAuth, async (req, res) => {
@@ -79,6 +102,28 @@ router.put("/:id", confirmAuth, async (req, res) => {
         runValidators: true,
       });
 
+      res.redirect("/dashboard");
+    }
+  } catch (err) {
+    console.error(err);
+    return res.render("error/500");
+  }
+});
+
+// @desc    Delete story
+// @route   DELETE /stories/:id
+router.delete("/:id", confirmAuth, async (req, res) => {
+  try {
+    let story = await Story.findById(req.params.id).lean();
+
+    if (!story) {
+      return res.render("error/404");
+    }
+
+    if (story.user != req.user.id) {
+      res.redirect("/stories");
+    } else {
+      await Story.remove({ _id: req.params.id });
       res.redirect("/dashboard");
     }
   } catch (err) {
