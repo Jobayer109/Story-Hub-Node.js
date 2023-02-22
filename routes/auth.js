60501b8610c28d9d6e30c4d602afc3e4445a8e9f
user.email <jobayer.ahmed109@gmail.com>
2023-02-22 15:03:34 +0600

    Another login action apply

--- routes/auth.js
+++ routes/auth.js
@@ -2,19 +2,36 @@ const express = require("express");
 const router = express.Router();
 const passport = require("passport");
 
+// router.get("/google", (req, res, next) => {
+//   const redirectUrl = req.query.redirectUrl;
+//   passport.authenticate("google", {
+//     callbackURL: redirectUrl,
+//     scope: ["profile"],
+//   })(req, res, next);
+// });
+
 router.get("/google", (req, res, next) => {
-  const redirectUrl = req.query.redirectUrl;
+  req.session.originalUrl = req.query.redirectUrl || "/";
   passport.authenticate("google", {
-    callbackURL: redirectUrl,
+    callbackURL: "/auth/google/callback",
     scope: ["profile"],
   })(req, res, next);
 });
 
+// router.get(
+//   "/google/callback",
+//   passport.authenticate("google", { failureRedirect: "/" }),
+//   (req, res) => {
+//     res.redirect("/dashboard");
+//   }
+// );
 router.get(
   "/google/callback",
   passport.authenticate("google", { failureRedirect: "/" }),
   (req, res) => {
-    res.redirect("/dashboard");
+    // Successful authentication, redirect to the original URL
+    const redirectUrl = req.session.originalUrl || "/";
+    res.redirect(redirectUrl);
   }
 );
 
