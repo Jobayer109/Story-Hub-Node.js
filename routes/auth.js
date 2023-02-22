d3cc79024d2f0de3e3aa419c8f4dab07a4f0ac5f
user.email <jobayer.ahmed109@gmail.com>
2023-02-22 15:12:42 +0600

    redirect issue solved

--- routes/auth.js
+++ routes/auth.js
@@ -2,36 +2,19 @@ const express = require("express");
 const router = express.Router();
 const passport = require("passport");
 
-// router.get("/google", (req, res, next) => {
-//   const redirectUrl = req.query.redirectUrl;
-//   passport.authenticate("google", {
-//     callbackURL: redirectUrl,
-//     scope: ["profile"],
-//   })(req, res, next);
-// });
-
 router.get("/google", (req, res, next) => {
-  req.session.originalUrl = req.query.redirectUrl || "/";
+  const redirectUrl = req.query.redirectUrl;
   passport.authenticate("google", {
-    callbackURL: "/auth/google/callback",
+    callbackURL: redirectUrl,
     scope: ["profile"],
   })(req, res, next);
 });
 
-// router.get(
-//   "/google/callback",
-//   passport.authenticate("google", { failureRedirect: "/" }),
-//   (req, res) => {
-//     res.redirect("/dashboard");
-//   }
-// );
 router.get(
   "/google/callback",
   passport.authenticate("google", { failureRedirect: "/" }),
   (req, res) => {
-    // Successful authentication, redirect to the original URL
-    const redirectUrl = req.session.originalUrl || "/";
-    res.redirect(redirectUrl);
+    res.redirect("/dashboard");
   }
 );
 
