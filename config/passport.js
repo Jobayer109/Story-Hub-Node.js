501aa09bde9e1467b1ed65a47daaa28e8d4df9b7
user.email <jobayer.ahmed109@gmail.com>
2023-02-22 14:06:25 +0600

    callback url changed

--- config/passport.js
+++ config/passport.js
@@ -9,7 +9,7 @@ module.exports = (passport) => {
       {
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL: "/auth/google/callback",
+        callbackURL: "https://${req.get('host')}/auth/google/callback",
       },
       async (accessToken, refreshToken, profile, done) => {
         const { id, displayName, name } = profile;
