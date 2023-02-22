ed1c32499ecb286b02557da466d5ec1de2090a0c
user.email <jobayer.ahmed109@gmail.com>
2023-02-22 14:24:39 +0600

    dynamic user login implemented

--- config/passport.js
+++ config/passport.js
@@ -9,7 +9,7 @@ module.exports = (passport) => {
       {
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL: "https://${req.get('host')}/auth/google/callback",
+        callbackURL: "/auth/google/callback",
       },
       async (accessToken, refreshToken, profile, done) => {
         const { id, displayName, name } = profile;
